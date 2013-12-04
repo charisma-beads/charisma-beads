e0379b9c3f4260603aefc1cef14e6eda98f75970
shaunfreeman <shaun@shaunfreeman.co.uk>
2013-12-04 18:30:18 +0000

    getting readuy for bootstrap 3

---
+++ module/Shop/src/Shop/Form/Product.php
@@ -0,0 +1,12 @@
+<?php
+namespace Shop\Form;
+
+use Zend\Form\Form;
+
+class Product extends Form
+{
+    public function __construct()
+    {
+        
+    }
+}
---
+++ module/Shop/src/Shop/InputFilter/Product.php
@@ -0,0 +1,12 @@
+<?php
+namespace Shop\InputFilter;
+
+use Zend\InputFilter\InputFilter;
+
+class Product extends InputFilter
+{
+    public function __construct()
+    {
+        
+    }
+}
