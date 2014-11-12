aa8f092acc739c4ca745c45ea4d48ce9f4122d7b
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-11-12 19:23:10 +0000

    updated admin theme

--- public/themes/admin/assets/js/LiveEditor/scripts/common/mediaelement/jquery.js
+++ public/themes/admin/assets/js/LiveEditor/scripts/common/mediaelement/jquery.js
@@ -465,7 +465,7 @@ jQuery.extend({
 			// A fallback to window.onload, that will always work
 			window.addEventListener( "load", jQuery.ready, false );
 
-		// If IE event model is used
+		// If IE event Model is used
 		} else if ( document.attachEvent ) {
 			// ensure firing before onload,
 			// maybe late but safe also for iframes
@@ -1486,7 +1486,7 @@ jQuery.support = (function() {
 
 	div.innerHTML = "";
 
-	// Figure out if the W3C box model works as expected
+	// Figure out if the W3C box Model works as expected
 	div.style.width = div.style.paddingLeft = "1px";
 
 	// We don't want to do body-related feature tests on frameset
