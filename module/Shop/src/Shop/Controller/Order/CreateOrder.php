b92b1872d90f045dba0d46acb7cffeed50c8b9e7
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-07-06 20:29:52 +0100

    some layout changes

---
+++ module/Shop/src/Shop/Controller/Order/CreateOrder.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Uthando CMS (http://www.shaunfreeman.co.uk/)
+ *
+ * @package   Shop\Controller\Order
+ * @author    Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @copyright Copyright (c) 2014 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license   see LICENSE.txt
+ */
+
+namespace Shop\Controller\Order;
+
+/**
+ * Class CreateOrder
+ *
+ * @package Shop\Controller\Order
+ */
+class CreateOrder
+{
+
+}
