f4169778acd8a258271c4e746e99f4d3c5163cf0
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-09-02 20:12:44 +0100

    added settings trait

--- module/Shop/src/Shop/Controller/Settings.php
+++ module/Shop/src/Shop/Controller/Settings.php
@@ -10,9 +10,8 @@
 
 namespace Shop\Controller;
 
+use UthandoCommon\Controller\SettingsTrait;
 use Zend\Mvc\Controller\AbstractActionController;
-use Zend\Http\PhpEnvironment\Response;
-use Zend\Config\Writer\PhpArray;
 
 /**
  * Class Settings
@@ -21,36 +20,11 @@ use Zend\Config\Writer\PhpArray;
  */
 class Settings extends AbstractActionController
 {
-    public function indexAction()
+    use SettingsTrait;
+
+    public function __construct()
     {
-        $form = $this->getServiceLocator()
-            ->get('FormElementManager')
-            ->get('ShopSettings');
-        
-        $prg = $this->prg();
-        
-        $config = $this->getServiceLocator()->get('config');
-        $settings = $config['shop'];
-        
-        if ($prg instanceof Response) {
-            return $prg;
-        } elseif (false === $prg) {
-            $form->setData($settings);
-            return ['form' => $form,]; 
-        }
-        
-        $form->setData($prg);
-        
-        if ($form->isValid()) {
-            $array = $form->getData();
-            unset($array['button-submit']);
-            
-            $config = new PhpArray();
-            $config->toFile('./config/autoload/shop.local.php', ['shop' => $array]);
-            
-            $this->flashMessenger()->addSuccessMessage('Settings have been updated!');
-        }
-        
-        return ['form' => $form,];
+        $this->setFormName('ShopSettings')
+            ->setConfigKey('shop');
     }
 }
