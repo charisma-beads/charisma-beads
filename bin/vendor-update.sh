0f50856bf3d457f1e4a4878b6801186f3f7f9f9a
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-11-16 08:40:24 +0000

    fixed vendor update script

--- bin/vendor-update.sh
+++ bin/vendor-update.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ../
 
-/usr/local/php7/bin/php ./composer.phar self-update
-/usr/local/php7/bin/php ./composer.phar --no-dev install
+/usr/local/php7/bin/php composer.phar self-update
+/usr/local/php7/bin/php composer.phar --no-dev install
