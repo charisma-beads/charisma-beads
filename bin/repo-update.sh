f6d0a93704c79927a00de572ab169e6942c3ed90
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-01-03 13:19:32 +0000

    fixed tag checkout for versions

--- bin/repo-update.sh
+++ bin/repo-update.sh
@@ -9,5 +9,5 @@ git fetch --tags
 CB_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo ${CB_VERSION^^}
 
-git checkout ${CB__VERSION^^}
+git checkout tags/${CB__VERSION^^}
 git pull
