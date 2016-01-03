12de01882a95ef5dc33d8c7de707a288f432a2df
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-01-03 13:12:08 +0000

    added fetch tags to repo update

--- bin/repo-update.sh
+++ bin/repo-update.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ../
 
+git fetch --tags
+
 CB_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo ${CB_VERSION^^}
 
