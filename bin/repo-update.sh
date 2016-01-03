381e719ddb9f4d72570951aa122071cd996cfb35
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-01-03 13:27:16 +0000

    update CB Version string

--- bin/repo-update.sh
+++ bin/repo-update.sh
@@ -7,7 +7,7 @@ cd ../
 git fetch --tags
 
 CB_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo ${CB_VERSION^^}
+echo ${CB_VERSION}
 
-git checkout tags/${CB__VERSION^^}
+git checkout tags/"${CB__VERSION}"
 git pull
