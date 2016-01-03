fad0e79692cdd18e7aedd05bc6a50c1c1129794f
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-01-03 12:38:59 +0000

    updated update scripts

--- bin/repo-update.sh
+++ bin/repo-update.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ../
 
-git stash
-git stash drop
+CB_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+echo ${CB_VERSION^^}
+
+git checkout ${CB__VERSION^^}
 git pull
