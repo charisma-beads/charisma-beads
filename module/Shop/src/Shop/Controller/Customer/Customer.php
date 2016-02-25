d05bd8afcede05093a0a4b546974456eeb9accce
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-02-25 15:24:47 +0000

    fixing errors on displaying forms

--- module/Shop/src/Shop/Controller/Customer/Customer.php
+++ module/Shop/src/Shop/Controller/Customer/Customer.php
@@ -5,7 +5,7 @@
  * @package   Shop\Controller\Customer
  * @author    Shaun Freeman <shaun@shaunfreeman.co.uk>
  * @copyright Copyright (c) 2014 Shaun Freeman. (http://www.shaunfreeman.co.uk)
- * @license   see LICENSE.txt
+ * @license   see LICENSE
  */
 
 namespace Shop\Controller\Customer;
@@ -24,10 +24,10 @@ use Zend\View\Model\ViewModel;
  */
 class Customer extends AbstractCrudController
 {
-	protected $controllerSearchOverrides = array('sort' => 'name');
-	protected $serviceName = 'ShopCustomer';
-	protected $userRoute = 'shop/customer';
-	protected $route = 'admin/shop/customer';
+    protected $controllerSearchOverrides = array('sort' => 'name');
+    protected $serviceName = 'ShopCustomer';
+    protected $userRoute = 'shop/customer';
+    protected $route = 'admin/shop/customer';
     protected $routes = [
         'edit' => 'admin/shop/customer/edit',
     ];
@@ -65,15 +65,15 @@ class Customer extends AbstractCrudController
             $result = $service->setCustomerAddress($post);
 
             return $jsonModel->setVariables([
-                'status'    => ($result) ? 'success' : 'danger',
-                'messages'  => ($result) ?
+                'status' => ($result) ? 'success' : 'danger',
+                'messages' => ($result) ?
                     'Address was saved to database' :
                     'Could not save address due to an error',
             ]);
         } catch (\Exception $e) {
             return $jsonModel->setVariables([
-                'status'    => 'danger',
-                'messages'  => $e->getMessage(),
+                'status' => 'danger',
+                'messages' => $e->getMessage(),
             ]);
         }
     }
@@ -81,46 +81,46 @@ class Customer extends AbstractCrudController
     /**
      * @return array|\Zend\Http\Response
      */
-	public function myDetailsAction()
-	{
-	    $prg = $this->prg($this->userRoute);
-	    
-	    /* @var $service \Shop\Service\Customer */
-	    $service = $this->getService();
-	    $userId = $this->identity()->getUserId();
-	    
-	    $customer = $service->getCustomerByUserId($userId);
-	    
-	    if ($prg instanceof Response) {
-	    	// returned a response to redirect us
-	    	return $prg;
-	    } elseif ($prg === false) {
-	    	// this wasn't a POST request, but there were no params in the flash messenger
-	    	// probably this is the first time the form was loaded
-	    	return [
-	    	  'form' => $this->getService()->getForm($customer),
-	    	  'model' => $customer,
+    public function myDetailsAction()
+    {
+        $prg = $this->prg($this->userRoute);
+
+        /* @var $service \Shop\Service\Customer */
+        $service = $this->getService();
+        $userId = $this->identity()->getUserId();
+
+        $customer = $service->getCustomerByUserId($userId);
+
+        if ($prg instanceof Response) {
+            // returned a response to redirect us
+            return $prg;
+        } elseif ($prg === false) {
+            // this wasn't a POST request, but there were no params in the flash messenger
+            // probably this is the first time the form was loaded
+            return [
+                'form' => $this->getService()->prepareForm($customer),
+                'model' => $customer,
             ];
-	    }
-	    
-	    $result = $service->edit($customer, $prg);
-	    
-	    if ($result instanceof Form) {
-	    	 
-	    	$this->flashMessenger()->addInfoMessage(self::FORM_ERROR);
-	    	 
-	    	return [
-    			'form'	=> $result,
-    			'model'	=> $customer,
-			];
-	    }
-	    
-	    if ($result) {
-	    	$this->flashMessenger()->addSuccessMessage('Your changes were saved.');
-	    } else {
-	    	$this->flashMessenger()->addErrorMessage('Your changes could not be save due to database error.');
-	    }
-	    
-	    return $this->redirect()->toRoute($this->userRoute);
-	}
+        }
+
+        $result = $service->edit($customer, $prg);
+
+        if ($result instanceof Form) {
+
+            $this->flashMessenger()->addInfoMessage(self::FORM_ERROR);
+
+            return [
+                'form' => $result,
+                'model' => $customer,
+            ];
+        }
+
+        if ($result) {
+            $this->flashMessenger()->addSuccessMessage('Your changes were saved.');
+        } else {
+            $this->flashMessenger()->addErrorMessage('Your changes could not be save due to database error.');
+        }
+
+        return $this->redirect()->toRoute($this->userRoute);
+    }
 }
