b29d9705ad1181d5e168294072baaf13b16b2699
shaunfreeman <shaun@shaunfreeman.co.uk>
2018-03-07 14:48:22 +0000

    added filters

--- module/Shop/src/Shop/InputFilter/Order/Order.php
+++ module/Shop/src/Shop/InputFilter/Order/Order.php
@@ -19,5 +19,42 @@ use Zend\InputFilter\InputFilter;
  */
 class Order extends InputFilter
 {
-    
+    public function init()
+    {
+        $this->add([
+            'name' => 'customerId',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+        ]);
+
+        $this->add([
+            'name' => 'orderId',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+        ]);
+
+        $this->add([
+            'name' => 'orderNumber',
+            'required' => false,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+        ]);
+
+        $this->add([
+            'name' => 'requirements',
+            'required' => false,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+        ]);
+    }
 }
