25c31c619904b6f3a38bd21d412766ad62da222a
shaunfreeman <shaun@shaunfreeman.co.uk>
2019-05-13 12:05:34 +0100

    fixed product spreadsheet error

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -8,8 +8,8 @@ return [
         'error_reporting'               => E_ALL ^ E_USER_DEPRECATED,
         'max_execution_time'            => 300,
         'date.timezone'                 => 'Europe/London',
-        'zlib.output_compression'       => true,
-        'zlib.output_compression_level' => -1,
+        //'zlib.output_compression'       => true,
+        //'zlib.output_compression_level' => -1,
     ],
     'uthando_user' => [
         'default_admin_route' => 'admin/shop',
