2b18985ead76a8f73f53e3af8f76a34c4a0947b0
shaunfreeman <shaun@shaunfreeman.co.uk>
2019-12-17 19:07:45 +0000

    global settings updated

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,8 +4,8 @@ return [
     'load_uthando_configs' => true,
     'php_settings' => [
         'display_startup_errors'        => false,
-        'display_errors'                => true,
-        'error_reporting'               => E_ALL ^ E_USER_DEPRECATED,
+        'display_errors'                => false,
+        'error_reporting'               => E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED & ~E_STRICT,
         'max_execution_time'            => 300,
         'date.timezone'                 => 'Europe/London',
         //'zlib.output_compression'       => true,
