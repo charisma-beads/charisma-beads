4b0c3f41cc3cd4c703f1ef32ef1cab0d574d113c
shaunfreeman <shaun@shaunfreeman.co.uk>
2020-07-28 17:08:27 +0100

    update user acl

--- config/autoload/user.global.php
+++ config/autoload/user.global.php
@@ -16,6 +16,12 @@ return [
                                     'login',  'forgot-password',
                                 ]],
                                 Application\Controller\IndexController::class => ['action' => ['index']],
+                                User\Controller\UserController::class => ['action' => [
+                                    'register', 'thank-you', 'login', 'authenticate', 'forgot-password'
+                                ]],
+                                User\Controller\RegistrationController::class => ['action' => [
+                                    'verify-email',
+                                ]],
                             ],
                         ],
                     ],
@@ -24,9 +30,22 @@ return [
                     'label' => 'User',
                     'parent' => 'guest',
                     'privileges' => [
+                        'deny' => [
+                            'controllers' => [
+                                User\Controller\UserController::class => ['action' => [
+                                    'register', 'thank-you', 'login', 'authenticate', 'forgot-password'
+                                ]],
+                                User\Controller\RegistrationController::class => ['action' => [
+                                    'verify-email',
+                                ]],
+                            ],
+                        ],
                         'allow' => [
                             'controllers' => [
                                 Application\Controller\IndexController::class => ['action' => ['index']],
+                                User\Controller\UserController::class => [
+                                    'action' => ['edit', 'password', 'logout']
+                                ]
                             ],
                         ],
                     ]
@@ -47,6 +66,14 @@ return [
                                 Admin\Controller\IndexController::class => ['action' => [
                                     'index', 'logout', 'password', 'profile',
                                 ]],
+                                User\Controller\AdminController::class => ['action' => 'all'],
+                                User\Controller\AdminRegistrationController::class => [
+                                    'action' => ['index', 'list', 'delete']
+                                ],
+                                User\Controller\LimitLoginController::class => [
+                                    'action' => ['index', 'list', 'delete']
+                                ],
+                                User\Controller\SettingsController::class => ['action' => 'all'],
                             ],
                         ],
                     ],
@@ -54,7 +81,13 @@ return [
             ],
             'resources' => [
                 Admin\Controller\IndexController::class,
-                Application\Controller\IndexController::class
+                Application\Controller\IndexController::class,
+                User\Controller\AdminController::class,
+                User\Controller\AdminRegistrationController::class,
+                User\Controller\LimitLoginController::class,
+                User\Controller\RegistrationController::class,
+                User\Controller\SettingsController::class,
+                User\Controller\UserController::class,
             ],
         ],
     ],
