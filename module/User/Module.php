40f647d94cfe8c5d5d1649fb2502b065f4111333
shaunfreeman <shaun@shaunfreeman.co.uk>
2020-08-01 13:48:38 +0100

    moved User module to PSR4

--- module/User/Module.php
+++ module/User/Module.php
@@ -30,21 +30,6 @@ class Module implements ConfigInterface, ConsoleUsageProviderInterface
         return include __DIR__ . '/config/module.config.php';
     }
 
-    /**
-     * @return array
-     */
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Laminas\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
-    }
-
-
     public function getConsoleUsage(Console $console)
     {
         return [
