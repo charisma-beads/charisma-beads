02fff0dd32d431d76e17357f766c9edb9c3a4857
shaunfreeman <shaun@shaunfreeman.co.uk>
2020-09-05 18:11:05 +0100

    changed serializer

--- module/Common/src/Hydrator/Strategy/Serialize.php
+++ module/Common/src/Hydrator/Strategy/Serialize.php
@@ -4,13 +4,25 @@ namespace Common\Hydrator\Strategy;
 
 use Laminas\Serializer\Serializer;
 use Laminas\Hydrator\Strategy\StrategyInterface;
+use Zumba\JsonSerializer\Exception\JsonSerializerException;
+use Zumba\JsonSerializer\JsonSerializer;
 
 
 class Serialize implements StrategyInterface
 {
+    /**
+     * @var JsonSerializer
+     */
+    protected $serializer;
+
+    public function __construct()
+    {
+        $this->serializer = new JsonSerializer();
+    }
+
     public function extract($value)
     {
-        return Serializer::serialize($value);
+        return $this->serializer->serialize($value);
     }
 
     public function hydrate($value)
@@ -18,7 +30,13 @@ class Serialize implements StrategyInterface
         if (!is_string($value)) {
             return $value;
         }
-        
-        return Serializer::unserialize($value);
+
+        try {
+            $value = $this->serializer->unserialize($value);
+        } catch (JsonSerializerException $e) {
+            $value = Serializer::unserialize($value);
+        }
+
+        return $value;
     }
 }
